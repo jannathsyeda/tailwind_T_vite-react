079869d829af7fe70ecb66735bd21dfa024e4d61
jannathsyeda <jannathsyeda7@gmail.com>
2024-05-02 12:22:07 +0600

    tailwind react configuration setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+       <h1>hello world</h1> 
+       </div>
+    </>
+  )
+}
+
+export default App
