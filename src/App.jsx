8ced218951220796d52a3482d420a7c4e73aa4e5
jannathsyeda <jannathsyeda7@gmail.com>
2024-05-03 13:46:50 +0600

    tailwind css applied for practice

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,19 @@ function App() {
       <div className="text-3xl font-bold underline">
        <h1>hello world</h1> 
        </div>
+       <div>
+        <p className="text-red-500">hello 1 </p>
+        <p className="text-yellow-500 text-4xl text-center">hello 1 </p>
+
+        <p>hello 1 </p>
+
+       </div>
+       <p className="text-red-500 bg-gray-700 h-32 w-1/2">hello tailwind css  </p>
+       <div >hello 30</div>
+       <div className="bg-[url('https://avatars.githubusercontent.com/u/59250850?v=4')] bg-center bg-no-repeat bg-cover h-screen w-1/2">
+image
+       </div>
+      
     </>
   )
 }
